06f7d5c7ac973a923467125c9ddd2cb09e1c17be
Joshua Carmack <joshuacarmack@users.noreply.github.com>
2026-01-08 20:19:37 -0500

    Added POTA event

---
+++ src/content/events/2026-01-17-pota.d
@@ -0,0 +1,11 @@
+---
+title: Parks on the Air (POTA) Outing
+date: "2026-01-17T11:00:00"
+endDate: "2026-01-17T14:00:00"
+location: Natural Tunnel State Park
+summary: Club outing for Parks on the Air.
+cover: /images/activities/POTA_Logo.png
+featured: false
+---
+
+Club outing for Parks on the air. For any questions, please contact Rick Curtis (W4BXL) at w4bxl@w4trc.org
